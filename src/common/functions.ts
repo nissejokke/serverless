5daec288d44a907605985b398355b2376f244929
Johan Sundberg <johan.sundberg@futureordering.com>
2021-08-06 15:30:44 +0200

    Case sensitive userId matching. Kube ingress host.

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -12,7 +12,7 @@ export interface Func extends FuncId {
 
 export async function getFunction(func: FuncId): Promise<Func> {
     const { functionId, userId } = func;
-    const result = await client.query(`SELECT functionId, userId, lastAccessed, lastUpdated FROM Functions WHERE functionId = ? AND userId = ?`, [functionId, userId]);
+    const result = await client.query(`SELECT functionId, userId, lastAccessed, lastUpdated FROM Functions WHERE functionId = ? AND BINARY userId = ?`, [functionId, userId]);
     return result[0];
 }
 
@@ -23,19 +23,19 @@ export async function createFunction(func: FuncId): Promise<void> {
 
 export async function accessFunction(func: FuncId): Promise<void> {
     const { functionId, userId } = func;
-    await client.execute(`UPDATE Functions SET lastAccessed = ? WHERE functionId = ? AND userId = ?`, [new Date(), functionId, userId]);
+    await client.execute(`UPDATE Functions SET lastAccessed = ? WHERE functionId = ? AND BINARY userId = ?`, [new Date(), functionId, userId]);
 }
 
 export async function updateFunction(func: FuncId): Promise<void> {
     const { functionId, userId } = func;
-    await client.execute(`UPDATE Functions SET lastUpdated = ? WHERE functionId = ? AND userId = ?`, [new Date(), functionId, userId]);
+    await client.execute(`UPDATE Functions SET lastUpdated = ? WHERE functionId = ? AND BINARY userId = ?`, [new Date(), functionId, userId]);
 }
 
 export async function deleteFunction(func: FuncId): Promise<void> {
     const { functionId, userId } = func;
-    await client.execute(`DELETE FROM Functions WHERE functionId = ? AND userId = ?`, [functionId, userId]);
+    await client.execute(`DELETE FROM Functions WHERE functionId = ? AND BINARY userId = ?`, [functionId, userId]);
 }
 
 export function getFunctions(userId: string): Promise<Func[]> {
-    return client.query(`SELECT functionId, userId, lastAccessed, lastUpdated FROM Functions WHERE userId = ?`, [userId]);
+    return client.query(`SELECT functionId, userId, lastAccessed, lastUpdated FROM Functions WHERE BINARY userId = ?`, [userId]);
 }
